f484ad2fc37edccb4230249b9c76f82563978040
Sanat02 <86793142+Sanat02@users.noreply.github.com>
2022-07-28 14:23:50 +0600

    Update SalaryCalculator.java

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,21 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+          if(salary<=10000)
+        {
+            System.out.println(salary-((salary/100)*15));
+        }
+        else if(salary>10000&&salary<=20000)
+        {
+            System.out.println(salary-((salary/100)*18));
+        }
+        else if(salary>20000)
+        {
+            System.out.println(salary-((salary/100)*20));
+        }
+        else
+        {
+            System.out.println("wrong input!");
+        }
     }
 }
