ae5340a45f350fc3e7bdfef5c4a6abc8b0b109f8
Sanat02 <86793142+Sanat02@users.noreply.github.com>
2022-07-28 16:23:21 +0600

    Update IsEnglishSymbolDeterminer.java

--- src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
@@ -2,13 +2,13 @@ package school.mjc.stage0.conditions.task3;
 
 public class IsEnglishSymbolDeterminer {
     public void isEnglishSymbol(char symbol) {
-       /* if(symbol>='A'&&symbol<='Z'||symbol>='a'&&symbol<='z')
+        if(symbol>='A'&&symbol<='Z'||symbol>='a'&&symbol<='z')
      {
          System.out.println("English");
      }
      else
      {
          System.out.println("Non English");
-     }*/
+     }
     }
 }
