a11eb3845e1d09c710169c2e6fd407d9a4e83742
Sanat02 <86793142+Sanat02@users.noreply.github.com>
2022-07-28 16:26:50 +0600

    Update Seasons.java

--- src/main/java/school/mjc/stage0/conditions/task3/Seasons.java
+++ src/main/java/school/mjc/stage0/conditions/task3/Seasons.java
@@ -2,7 +2,7 @@ package school.mjc.stage0.conditions.task3;
 
 public class Seasons {
     public void tellTheSeasonByMonthNumber(int month) {
-      /*  if(month==1||month==2||month==12)
+        if(month==1||month==2||month==12)
        {
            System.out.println("Winter");
        }
@@ -21,7 +21,7 @@ public class Seasons {
        else
        {
            System.out.println("Wrong number!");
-       }*/
+       }
 
     }
 }
